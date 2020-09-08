a8c97a33fe5af91ed24ddb5061f52a9f617e57fd
vixelambda <dir.victor@yandex.ru>
2020-09-08 10:26:02 +0300

    practice3

---
+++ practice3-Директоров/src/pr3/Circle.java
@@ -0,0 +1,43 @@
+package pr3;
+
+public class Circle extends Shape{
+    protected double radius;
+
+    public Circle() {}
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 3.14*radius*radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*3.14*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ practice3-Директоров/src/pr3/Square.java
@@ -0,0 +1,51 @@
+package pr3;
+
+public class Square extends Rectangle{
+    protected double side;
+    public Square() {}
+    public Square(double side) {
+        this.side = side;
+    }
+    public Square(double side, String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public void setWidth(double side) {
+        this.width = side;
+        this.length = side;
+    }
+
+    public void setLength(double side) {
+        this.length = side;
+        this.width = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side*side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
