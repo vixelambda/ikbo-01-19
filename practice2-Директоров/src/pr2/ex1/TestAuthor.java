c77e035cf429bbb23d44658a1372128eb8aff68c
vixelambda <dir.victor@yandex.ru>
2020-09-08 10:20:38 +0300

    practice2

---
+++ practice2-Директоров/src/pr2/ex1/TestAuthor.java
@@ -0,0 +1,8 @@
+package pr2.ex1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author ("Stephen King", "sk@mail.ru", 'm');
+        System.out.println(a1);
+    }
+}
